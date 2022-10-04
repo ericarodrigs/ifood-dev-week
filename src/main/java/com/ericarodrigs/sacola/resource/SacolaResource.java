351e2d1bc62889c3581f0088b973b540dbc9f812
Erica Rodrigues <ericamrodrigs@gmail.com>
2022-10-03 21:50:41 -0300

    Creates Sacola Resource

---
+++ src/main/java/com/ericarodrigs/sacola/resource/SacolaResource.java
@@ -0,0 +1,31 @@
+package com.ericarodrigs.sacola.resource;
+
+import com.ericarodrigs.sacola.model.Item;
+import com.ericarodrigs.sacola.model.Sacola;
+import com.ericarodrigs.sacola.resource.dto.ItemDto;
+import com.ericarodrigs.sacola.service.SacolaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ifood-devweek/sacolas")
+@RequiredArgsConstructor
+public class SacolaResource {
+    private final SacolaService sacolaService;
+
+    @PostMapping
+    public Item incluirItemNaSacola(@RequestBody ItemDto itemDto) {
+        return sacolaService.incluirItemNaSacola(itemDto);
+    }
+
+    @GetMapping("/{id}")
+    public Sacola verSacola(@PathVariable("id") Long id){
+        return sacolaService.verSacola(id);
+    }
+
+    @PatchMapping("/fecharSacola/{sacolaId}")
+    public Sacola fecharSacola(@PathVariable("sacolaId") Long sacolaId,
+                               @RequestParam("formaPagamento") int formaPagamento){
+        return sacolaService.fecharSacola(sacolaId, formaPagamento);
+    }
+}
