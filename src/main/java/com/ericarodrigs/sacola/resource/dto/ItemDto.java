3360ce7b53adf5fc56a1ce1d98d5eca8c6c82784
Erica Rodrigues <ericamrodrigs@gmail.com>
2022-10-03 21:48:57 -0300

    Creates ItemDto

---
+++ src/main/java/com/ericarodrigs/sacola/resource/dto/ItemDto.java
@@ -0,0 +1,21 @@
+package com.ericarodrigs.sacola.resource.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@AllArgsConstructor
+@Builder
+@Data
+@Embeddable
+@NoArgsConstructor
+
+public class ItemDto {
+    private Long produtoId;
+    private int quantidade;
+    private Long idSacola;
+}
